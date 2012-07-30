af3564ef52fdc0fde3b5b942173eef5e3ec6f422
Tom Ashworth <tom@phuu.net>
2012-07-30 16:01:07 +0300

    Added manual cache invalidation

--- lib/buffer.rb
+++ lib/buffer.rb
@@ -23,7 +23,6 @@ module Buffer
 
       @conn = Faraday.new :url => 'https://api.bufferapp.com/1/'
       @addr = Addressable::URI.new
-      @cache = {}
     end
 
     # get is a shorthand method for api :get
@@ -69,16 +68,39 @@ module Buffer
 
   class User < Client
 
+    def initialize(token)
+      super
+      invalidate
+    end
+
     private
 
+      # user is a method for handling the cache of the user
+      # data from the Buffer API.
+      #
+      # Returns a hash of user data.
       def user
         @cache[:user] ||= get 'user'
       end
 
     public
 
+      # invalidate wipes the cache so that future requests
+      # rebuild it from server data
+      def invalidate
+        @cache = {}
+      end
+
+      # method_missing steps in to enable the helper methods
+      # by trying to get a particular key from self.user
+      #
+      # Returns the user data or the result of super
       def method_missing(method, *args, &block)
         user[method.to_s] || super
+      end
+
+      def respond_to?(name)
+        user.key_exist? name
       end
 
   end
--- spec/buffer_spec.rb
+++ spec/buffer_spec.rb
@@ -289,16 +289,62 @@ describe Buffer::User do
           :headers => {:content_type => 'application/json; charset=utf-8'})
     end
 
-    it 'should respond with correct id' do
+    it 'respond with correct id' do
       subject.id.should eq('1234')
     end
 
-    it 'should not respond to eye_color' do
+    it 'do not respond to eye_color' do
       lambda { color = subject.eye_color }.should raise_error
     end
 
-    it 'should respond to get' do
+    it 'respond to get' do
       lambda { user = subject.get 'user' }.should_not raise_error
+    end
+
+  end
+
+  describe 'cache' do
+
+    subject do
+      Buffer::User.new 'some_token'
+    end
+
+    before do
+      stub_get('user.json').
+        with(:query => {:access_token => 'some_token'}).
+        to_return(
+          :body => fixture('user.json'),
+          :headers => {:content_type => 'application/json; charset=utf-8'})
+    end
+
+    describe 'access' do
+
+      before do
+        subject.id
+      end
+
+      it 'is used after accessing id once' do
+        id = subject.id
+        a_get('user.json').
+          should_not have_been_made
+      end
+
+    end
+
+    describe 'invalidation' do
+
+      before do
+        subject.id
+      end
+
+      it 'forces server access' do
+        subject.invalidate
+        id = subject.id
+        a_get('user.json').
+          with(:query => {:access_token => 'some_token'}).
+          should have_been_made.times(2)
+      end
+
     end
 
   end
